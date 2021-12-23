66a66a3cb370ce2d6cd78e93b7d2d510221971ea
Bekir Yalcin <70541124+siberblog@users.noreply.github.com>
2021-12-23 14:23:39 +0000

    Duplicate

--- src/index.js
+++ src/index.js
@@ -27,6 +27,36 @@ const gracefulShutdown = () => {
         .then(() => process.exit()); 
 };
 
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit()); 
+};
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit()); 
+};
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit()); 
+};
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit()); 
+};
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit()); 
+};
+
 process.on('SIGINT', gracefulShutdown);
 process.on('SIGTERM', gracefulShutdown);
 process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
